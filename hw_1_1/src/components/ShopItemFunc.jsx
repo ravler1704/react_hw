9d29fe117428207c2143e6265a5cbead3547994e
Alexandr <avr1704@yandex.ru>
2023-07-25 11:14:28 +0300

    hw_1_1 func component instead of class component

--- hw_1_1/src/components/ShopItemFunc.jsx
+++ hw_1_1/src/components/ShopItemFunc.jsx
@@ -1,30 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-
-
-class ShopItemFunc extends Component {
-    render() {
-        let price = (this.props.item.price).toLocaleString(
-          'en-US',
-          { minimumFractionDigits: 2 }
-        );
-        return (
-            <div className='main-content'>
-              <h2>{this.props.item.brand}</h2>
-              <h1>{this.props.item.title}</h1>
-              <h3>{this.props.item.description}</h3>
-              <div className='description'>{this.props.item.descriptionFull}</div>
-              <div className="highlight-window mobile">
+function ShopItemFunc(props) {
+    let price = (props.item.price).toLocaleString(
+        'en-US',
+        { minimumFractionDigits: 2 }
+    );
+    return (
+        <div className='main-content'>
+            <h2>{props.item.brand}</h2>
+            <h1>{props.item.title}</h1>
+            <h3>{props.item.description}</h3>
+            <div className='description'>{props.item.descriptionFull}</div>
+            <div className="highlight-window mobile">
                 <div className="highlight-overlay"></div>
-              </div>
-              <div className="divider"></div>
-              <div className="purchase-info">
-                <div className="price">{this.props.item.currency}{price}</div>
-                <button>Добавить в корзину</button>
-              </div>
             </div>
-        );
-    }
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{props.item.currency}{price}</div>
+                <button>Добавить в корзину</button>
+            </div>
+        </div>
+    );
 }
 
 export default ShopItemFunc
