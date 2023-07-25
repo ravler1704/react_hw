d0a0272d3683bd64fd92572429ad702c64245fed
Alexandr <avr1704@yandex.ru>
2023-07-25 15:42:07 +0300

    hw_1_2

---
+++ hw_1_2/src/components/ShopItemClass.jsx
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class ShopItemClass extends Component {
+    render() {
+        let price = (this.props.item.price).toLocaleString(
+            'en-US',
+            { minimumFractionDigits: 2 }
+        );
+        return (
+            <div>
+                <div className='main-content'>
+                    <h2>{this.props.item.brand}</h2>
+                    <h1>{this.props.item.title}</h1>
+                    <h3>{this.props.item.description}</h3>
+                    <div className='description'>{this.props.item.descriptionFull}</div>
+                    <div className="highlight-window mobile">
+                        <div className="highlight-overlay"></div>
+                    </div>
+                    <div className="divider"></div>
+                    <div className="purchase-info">
+                        <div className="price">{this.props.item.currency}{price}</div>
+                        <button>Добавить в корзину</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ShopItemClass
